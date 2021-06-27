875ef8de7cbc1acc8852f88b1a8bdc398f98ee0a
Rahul255 <rahulmohan255@gmail.com>
2021-06-27 20:30:14 +0530

    file added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,24 @@ import ExpenseDate from './ExpenseDate';
 //props used to call value from other components
 const ExpenseItem = (props) => {
     //javascript code Here
+    let title = props.title;
+
+
+    const clickHandler = () => {//error function for click handler
+        console.log('Clicked');
+    }
     return(
         //this card is a custom buildable wrapper component 
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}$</div>
             </div>
+            
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
+        //upper button click function, we wiil create an anonyms error fun
     );
 }
 
