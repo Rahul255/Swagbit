75eb98bb96ad2d0344e9c0479c876fe4c0832e0b
Rahul255 <rahulmohan255@gmail.com>
2021-06-27 14:45:38 +0530

    file added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem () {
+//props used to call value from other components
+function ExpenseItem (props) {
+    //javascript code Here
     return(
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">294.5$</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}$</div>
             </div>
         </div>
     );
