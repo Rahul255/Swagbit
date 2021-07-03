4ce2954fc8991f83d4357f5bfa618a40a1deb4cf
Rahul255 <rahulmohan255@gmail.com>
2021-07-03 12:47:14 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -26,9 +26,16 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In app js');
+    console.log(expense);
+  };
+
   return (
+    //app component can access both child components 
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
